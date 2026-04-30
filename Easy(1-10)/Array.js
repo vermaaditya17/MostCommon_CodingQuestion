7a0ea6df138fdf9c4812bd74afa3ae3352c2cc7e
vermaaditya17 <145982553+vermaaditya17@users.noreply.github.com>
2026-04-30 16:55:31 +0530

    adding Question

--- Easy(1-10)/Array.js
+++ Easy(1-10)/Array.js
@@ -39,4 +39,69 @@ function reverseArray(arr){
 }
 
 // Count even number
-f
+function evenNumber(arr){
+     let count = 0;
+     for(let i = 0 ; i<arr.length; i++){
+          if(arr[i]%2==0){
+               count++
+          }
+
+     }
+     return count
+}
+// find second largest Element
+function secLargestElement(arr){
+     let first = - Infinity;
+     let second = -Infinity;
+
+     for(let i of arr){
+          if(i>first){
+               second = first
+               first = i;
+          }
+          else if(i > second && i!==first){
+               second = i;
+          }
+     }
+     return second
+}
+console.log(secLargestElement([1,2,3,4,5]))
+
+//find min element in the array
+function minElement(arr){
+    let min = Infinity ;
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]<min){  
+            min = arr[i]
+
+        }
+    }
+    return min
+
+}
+console.log(minElement([-1,2,-2,3,43]))
+
+
+// is array sorted in ascending order
+function isArraySorted(arr){
+    for(let i=0; i<=arr.length ; i++){
+        if(arr[i] > arr[i+1]){
+            return false
+        }
+    }
+    return true
+
+}
+console.log(isArraySorted([1,2,3,6,5]))
+
+//sum of element
+function sumElement(arr){
+    let sum = 0 
+    for(let i=0 ; i<arr.length; i++){
+        sum+=arr[i]
+    }
+    return sum 
+}
+console.log(sumElement([1,2,24,24,4,131,31,3,123,243,2,42342455,23,32,54,32,55,325,25,234,53,43,4,34,3
+    
+]))
