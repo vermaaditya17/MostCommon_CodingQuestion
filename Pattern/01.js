b36923203f47b9cad6e7e5253a8c33005430a41e
vermaaditya17 <145982553+vermaaditya17@users.noreply.github.com>
2026-05-01 15:30:25 +0530

    updated Question

--- Pattern/01.js
+++ Pattern/01.js
@@ -18,4 +18,26 @@ function RightTriangle(){
     }
 }
 
+function invertedTriangle(){
+    for(let i=4; i>0; i--){
+        let star=""
+        for(let j=1;j<=i; j++){
+            star+="* "
+        }
+        console.log(star)
+    }
+}
 
+function numPrintTriangle(){
+    for(let i=1; i<=4; i++){
+        let num = ""
+        for(let j=1; j<=i;j++){
+            num+=j + " "
+        }
+        console.log(num)
+    }
+}
+
+function pyramidPattern(){
+    
+}
