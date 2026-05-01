2eef62b1575e15733c9c82f36ce6134e9336b497
vermaaditya17 <145982553+vermaaditya17@users.noreply.github.com>
2026-05-01 15:10:27 +0530

    update file and Question

---
+++ Pattern/01.js
@@ -0,0 +1,10 @@
+function SquarePattern(){
+    for(let i=0;i<=4; i++){
+        let star = ""
+        for(let j=0; j<=4; j++){
+            star+="* "
+    }
+    console.log(star)
+}
+}
+SquarePattern()
